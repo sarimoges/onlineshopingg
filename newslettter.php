d3b68ddc47a8ed0bb0e669ccd0b23a27e8571bc3
wku-cslab1 <sarimoges>
2022-01-30 13:42:35 -0800

    Initial commit

---
+++ newslettter.php
@@ -0,0 +1,24 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
